5d53d6540d7cf8261cb8c612badcdbf9f7b527c3
angulawk <agnieszka.weronika.wojcik@gmail.com>
2019-04-04 22:35:31 +0700

    Rewrite jquery to javascript

--- js/main.js
+++ js/main.js
@@ -1,73 +1,94 @@
-$(document).ready(function() {
-  //toggle navigation
+(function() {
+  //Toggle navigation
 
-  $(".hamburger").click(function() {
-    $(this).toggleClass("is-active");
-    $(".links").toggleClass("active");
+  const hamburger = document.querySelector(".hamburger");
+  const links = document.querySelector(".links");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("is-active");
+    links.classList.toggle("active");
   });
 
-  //page scroll
+  //Website hover scroll
 
-  var xH;
-  $(".small_view").hover(
-    function() {
-      xH = $(this)
-        .children("img")
-        .css("height");
-      xH = parseInt(xH);
-      xH = xH - 350;
-      xH = "-" + xH + "px";
-      $(this)
-        .children("img")
-        .css("top", xH);
-    },
-    function() {
-      $(this)
-        .children("img")
-        .css("top", "0px");
+  let imageHeight;
+  const smallViewArray = document.querySelectorAll(".small_view");
+
+  for (let index = 0; index < smallViewArray.length; index++) {
+    let smallViewHeight = smallViewArray[index].clientHeight;
+    smallViewArray[index].addEventListener("mouseover", function() {
+      imageHeight = parseInt(this.childNodes[1].height);
+      const hiddenImageHeight = imageHeight - smallViewHeight;
+      imageHeight = `-${hiddenImageHeight}px`;
+      this.childNodes[1].style.top = imageHeight;
+    });
+
+    smallViewArray[index].addEventListener("mouseleave", function() {
+      this.childNodes[1].style.top = "0px";
+    });
+  }
+
+  //Open info show
+  const moreInfoArray = document.querySelectorAll(".more_info");
+
+  for (let index = 0; index < moreInfoArray.length; index++) {
+    moreInfoArray[index].addEventListener("click", function() {
+      const boxInfo = this.childNodes[3];
+      const headerContent = boxInfo.childNodes[1];
+      boxInfo.classList.toggle("show_box_info");
+
+      const isShowBoxInfoVisible = boxInfo.classList.length === 2;
+      isShowBoxInfoVisible
+        ? (boxInfo.style.height = `${headerContent.clientHeight}px`)
+        : (boxInfo.style.height = "0px");
+    });
+  }
+
+  //   $(function() {
+  //     $('a[href*="#"]:not([href="#"])').click(function() {
+  //       if (
+  //         location.pathname.replace(/^\//, "") ==
+  //           this.pathname.replace(/^\//, "") &&
+  //         location.hostname == this.hostname
+  //       ) {
+  //         var target = $(this.hash);
+  //         target = target.length
+  //           ? target
+  //           : $("[name=" + this.hash.slice(1) + "]");
+  //         if (target.length) {
+  //           $("html, body").animate(
+  //             {
+  //               scrollTop: target.offset().top
+  //             },
+  //             1000
+  //           );
+  //           return false;
+  //         }
+  //       }
+  //     });
+  //   });
+
+  //Fixed menu
+
+  window.addEventListener("scroll", function(e) {
+    const menu = document.querySelector("#menu");
+    if (window.scrollY > window.innerHeight / 2) {
+      menu.classList.add("fixed");
+    } else {
+      menu.classList.remove("fixed");
     }
-  );
-
-  //box info show
-  $(".triangle").click(function() {
-    var revealThing = $(this).next();
-    revealThing.slideToggle();
   });
 
-  $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (
-        location.pathname.replace(/^\//, "") ==
-          this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-      ) {
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $("[name=" + this.hash.slice(1) + "]");
-        if (target.length) {
-          $("html, body").animate(
-            {
-              scrollTop: target.offset().top
-            },
-            1000
-          );
-          return false;
-        }
-      }
+  //Active link
+
+  const linksList = document.querySelectorAll(".links li a");
+
+  linksList.forEach(function(item) {
+    item.addEventListener("click", function() {
+      linksList.forEach(function(element) {
+        element.classList.remove("active_link");
+      });
+      item.classList.add("active_link");
     });
   });
-
-  $(window).bind("scroll", function() {
-    if ($(window).scrollTop() > 50) {
-      $("#menu").addClass("fixed");
-    } else {
-      $("#menu").removeClass("fixed");
-    }
-  });
-
-  $(".links li a").click(function() {
-    $("li a").removeClass("active_link");
-    $(this).addClass("active_link");
-  });
-});
+})();
